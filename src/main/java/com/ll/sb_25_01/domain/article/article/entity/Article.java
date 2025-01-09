cc18f7ac49420f5d8a70bf817be55328d1f5eb91
kimhyeji <gpwl5589@gmail.com>
2025-01-09 11:57:02 +0900

    step 5, variadic parameter

--- src/main/java/com/ll/sb_25_01/domain/article/article/entity/Article.java
+++ src/main/java/com/ll/sb_25_01/domain/article/article/entity/Article.java
@@ -63,4 +63,10 @@ public class Article extends BaseEntity {
 
         tags.add(tag);
     }
+
+    public void addTag(String... tagContents) {
+        for (String tagContent : tagContents) {
+            addTag(tagContent);
+        }
+    }
 }
