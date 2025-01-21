1e9346c647528c6da79623424ed4d675e41599e5
kimhyeji <gpwl5589@gmail.com>
2025-01-21 10:15:35 +0900

    step 25, pagination 6

--- src/main/java/com/ll/sb_25_01/domain/article/article/controller/ArticleController.java
+++ src/main/java/com/ll/sb_25_01/domain/article/article/controller/ArticleController.java
@@ -49,6 +49,8 @@ public class ArticleController {
         Page<Article> itemsPage = articleService.search(kwTypes, kw, pageable);
         model.addAttribute("itemsPage", itemsPage);
         model.addAttribute("kwTypesMap", kwTypesMap);
+        model.addAttribute("page", page);
+
 
         return "article/list";
     }
